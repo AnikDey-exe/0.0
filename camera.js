6ab6f6febeeb26898dcad4a56cfad71d9a59458e
Anik-Dey.exe <67157750+AnikDey-exe@users.noreply.github.com>
2021-09-12 12:28:58 -0400

    Add files via upload

---
+++ camera.js
@@ -0,0 +1,85 @@
+import * as React from "react";
+import { Button, Image, View, Platform } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
+
+export default class PickImage extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            image: null
+        }
+    };
+
+    getPermissionAsync = async () => {
+        if (Platform.OS !== "web") {
+            const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+            if (status !== "granted") {
+                alert("Sorry, we need camera roll permissions to make this work!");
+            }
+        }
+    }
+
+    uploadImage = async (uri) => {
+        const data = new FormData();
+        let filename = uri.split("/")[uri.split("/").length - 1]
+        let type = `image/${uri.split('.')[uri.split('.').length - 1]}`
+        const fileToUpload = {
+            uri: uri,
+            name: filename,
+            type: type,
+        };
+        data.append("alphabet", fileToUpload);
+        fetch(" https://e2f1-2600-1700-3070-bc90-34da-2756-55b-6d9c.ngrok.io/predict-alphabet", {
+            method: "POST",
+            body: data,
+            headers: {
+                "content-type": "multipart/form-data",
+            },
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                console.log("Success:", result);
+            })
+            .catch((error) => {
+                //console.warn(xhr.responseText)
+                console.error("Error:", error);
+            });
+    }
+
+    _pickImage = async () => {
+        try {
+            let result = await ImagePicker.launchImageLibraryAsync({
+                mediaTypes: ImagePicker.MediaTypeOptions.All,
+                allowsEditing: true,
+                aspect: [4, 3],
+                quality: 1,
+            });
+            if (!result.cancelled) {
+                this.setState({ image: result.data });
+                console.log(result.uri);
+                console.log("Success:", result);
+                this.uploadImage(result.uri);
+            }
+        } catch (E) {
+            console.log(E);
+        }
+    }
+
+    componentDidMount() {
+        this.getPermissionAsync();
+    }
+
+    render() {
+        let { image } = this.state;
+
+        return (
+            <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+                <Button
+                    title="Pick an image from camera roll"
+                    onPress={this._pickImage}
+                />
+            </View>
+        );
+    }
+}
